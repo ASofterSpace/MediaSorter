39bc49ef9203959bae80aa3f322e4a69b528e3ab
Moya <moya@asofterspace.com>
2026-05-12 21:09:33 +0200

    Adjust title

--- src/com/asofterspace/mediaSorter/Main.java
+++ src/com/asofterspace/mediaSorter/Main.java
@@ -16,8 +16,8 @@ import com.asofterspace.toolbox.Utils;
 public class Main {
 
 	public final static String PROGRAM_TITLE = "Media Sorter";
-	public final static String VERSION_NUMBER = "0.0.2.6(" + Utils.TOOLBOX_VERSION_NUMBER + ")";
-	public final static String VERSION_DATE = "31. August 2019 - 7. May 2026";
+	public final static String VERSION_NUMBER = "0.0.2.7(" + Utils.TOOLBOX_VERSION_NUMBER + ")";
+	public final static String VERSION_DATE = "31. August 2019 - 12. May 2026";
 
 
 	public static void main(String[] args) {
